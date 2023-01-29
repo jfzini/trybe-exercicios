f0943dd2bfc988cc76403db50536bc8f00a67c9a
João Felipe Zini <joaofelipezini@gmail.com>
2023-01-29 19:32:48 -0300

    iniciando projeto pelo readme

---
+++ fundamentos/secao-3-introducao-a-javascript/dia-05-let-const-arrow-functions-template-literal/functions.js
@@ -0,0 +1,20 @@
+// COMEÇANDO ALGUNS REQUISITOS DO PROJETO//
+const girafa = true;
+const elefante = false;
+const macaco = false;
+
+
+const compareTrue = (param1, param2) => {
+  if (param1 == true && param2 == true) {
+    return true;
+  } else if (param1 == true && param2 == false) {
+    return false;
+  } else if (param2 == true && param1 == false) {
+    return false;
+  } else if (param1 == false && param2 == false) {
+    return false;
+  }
+  return 'Invalid';
+}
+
+console.log(compareTrue(macaco, elefante));
