8551e20b5484c66b7d56dbbb1898b4ae9a1b3f64
João Felipe Zini <joaofelipezini@gmail.com>
2023-03-16 12:47:14 -0300

    aplicação criada no course

--- front-end/introducao-frontend-js-assincrono/dia-01-ambiente-desenvolvimento/src/main.js
+++ front-end/introducao-frontend-js-assincrono/dia-01-ambiente-desenvolvimento/src/main.js
@@ -1,5 +1,8 @@
 import { nanoid } from "nanoid";
+import clipboardCopy from "clipboard-copy";
+import './style.css';
 
 const genButton = document.getElementById('gen-button');
 const generatedPW = document.getElementById('pw');
 genButton.addEventListener('click', () => generatedPW.innerHTML = nanoid());
+genButton.addEventListener('click', () => clipboardCopy(generatedPW.innerHTML));
