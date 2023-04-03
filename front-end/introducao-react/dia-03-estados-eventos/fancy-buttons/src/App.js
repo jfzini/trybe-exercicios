4a3d7159c5ddbda157c8a74dfd150a3a78f2f8a6
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-03 13:20:11 -0300

    testing state handling

--- front-end/introducao-react/dia-03-estados-eventos/fancy-buttons/src/App.js
+++ front-end/introducao-react/dia-03-estados-eventos/fancy-buttons/src/App.js
@@ -2,23 +2,35 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  
+  state = {
+    counter1: 0,
+    counter2: 0,
+    counter3: 0,
+  }
 
   handleClick1 = () => {
-    console.log(this);
+    this.setState((prevState, _props) => ({
+      counter1: prevState.counter1 + 1,
+    }))
   }
   handleClick2 = () => {
-    console.log(this);
+    this.setState((prevState, _props) => ({
+      counter2: prevState.counter2 + 1,
+    }))
   }
   handleClick3 = () => {
-    console.log(this);
+    this.setState((prevState, _props) => ({
+      counter3: prevState.counter3 + 1,
+    }))
   }
 
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleClick1}>CLIQUE 1</button>
-        <button onClick={this.handleClick2}>CLIQUE 2</button>
-        <button onClick={this.handleClick3}>CLIQUE 3</button>
+        <button onClick={this.handleClick1}>{this.state.counter1}</button>
+        <button onClick={this.handleClick2}>{this.state.counter2}</button>
+        <button onClick={this.handleClick3}>{this.state.counter3}</button>
       </div>
     );
   }
