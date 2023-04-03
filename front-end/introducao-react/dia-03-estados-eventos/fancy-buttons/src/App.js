f059df2da967dd65c23f8a8f230eee7f971d1034
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-02 22:42:50 -0300

    funções dentro da classe

--- front-end/introducao-react/dia-03-estados-eventos/fancy-buttons/src/App.js
+++ front-end/introducao-react/dia-03-estados-eventos/fancy-buttons/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './App.css';
 
-
-function handleClick1() {
-   console.log('CLIQUE 1');
-}
-function handleClick2() {
-   console.log('CLIQUE 2');
-}
-function handleClick3() {
-   console.log('CLIQUE 3');
-}
-
-
 class App extends React.Component {
+  handleClick1() {
+    console.log('CLIQUE 1');
+  }
+  handleClick2() {
+    console.log('CLIQUE 2');
+  }
+  handleClick3() {
+    console.log('CLIQUE 3');
+  }
+
   render() {
     return (
       <div className="App">
-        <button onClick={handleClick1}>CLIQUE 1</button>
-        <button onClick={handleClick2}>CLIQUE 2</button>
-        <button onClick={handleClick3}>CLIQUE 3</button>
+        <button onClick={this.handleClick1}>CLIQUE 1</button>
+        <button onClick={this.handleClick2}>CLIQUE 2</button>
+        <button onClick={this.handleClick3}>CLIQUE 3</button>
       </div>
     );
   }
