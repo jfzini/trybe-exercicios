6b5c3604a6573fc1608262c83505f0270eb167af
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-27 15:48:01 -0300

    getting to know mockReturnValue and mockReturnValueOnce

--- front-end/React-test-library/testes-react/src/Testing.test.js
+++ front-end/React-test-library/testes-react/src/Testing.test.js
@@ -6,10 +6,24 @@ const retornaNumeroAleatorio = () => Math.floor(Math.random() * 100);
 let divisivelPorDois = () => (retornaNumeroAleatorio() % 2) === 0;
 
 test("#divisivelPorDois", () => {
-  // testando se a função foi chamada e qual seu retorno
-  divisivelPorDois = jest.fn().mockReturnValue(true);
+  // testando quantas vezes a função foi chamada e qual seu retorno
+  divisivelPorDois = jest
+    .fn()
+    .mockReturnValue('default value')
+    .mockReturnValueOnce('first call')
+    .mockReturnValueOnce('second call');
 
-  divisivelPorDois();
+  expect(divisivelPorDois).toHaveBeenCalledTimes(0);
+
+  expect(divisivelPorDois()).toBe("first call");
   expect(divisivelPorDois).toHaveBeenCalledTimes(1);
-  expect(divisivelPorDois()).toBe(true);
+
+  expect(divisivelPorDois()).toBe("second call");
+  expect(divisivelPorDois).toHaveBeenCalledTimes(2);
+
+  expect(divisivelPorDois()).toBe("default value");
+  expect(divisivelPorDois).toHaveBeenCalledTimes(3);
+
+  expect(divisivelPorDois()).toBe("default value");
+  expect(divisivelPorDois).toHaveBeenCalledTimes(4);
 });
