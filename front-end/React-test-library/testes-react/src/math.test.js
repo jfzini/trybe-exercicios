2b558abb49b68ce383cb853c3f7d62e847c96bf4
João Felipe Zini <joaofelipezini@gmail.com>
2023-04-28 10:26:28 -0300

    mock spyOn other possibilities

--- front-end/React-test-library/testes-react/src/math.test.js
+++ front-end/React-test-library/testes-react/src/math.test.js
@@ -35,6 +35,30 @@ const math = require('./math');
 //   expect(math.somar).toHaveBeenLastCalledWith(1, 2);
 // });
 
+// test("#somar", () => {
+//   // testando a implementação original, o mock e a restauração da função original
+
+//   // implementação original
+//   expect(math.somar(1, 2)).resolves.toBe(3); //como é uma promessa, é necessário o resolves.
+
+//   // criando o mock e substituindo a implementação para uma subtração
+//   const mockSomar = jest
+//     .spyOn(math, "somar")
+//     .mockImplementation((a, b) => a - b);
+
+//   math.somar(5, 1);
+//   expect(mockSomar).toHaveBeenCalledTimes(1);
+//   expect(mockSomar(5, 1)).toBe(4);
+//   expect(mockSomar).toHaveBeenCalledTimes(2);
+//   expect(mockSomar).toHaveBeenLastCalledWith(5, 1);
+
+//   // restaurando a implementação original
+//   math.somar.mockRestore();
+//   expect(math.somar(1, 2)).resolves.toBe(3);
+// });
+
+//Outra possibilidade do spyOn
+
 test("#somar", () => {
   // testando a implementação original, o mock e a restauração da função original
 
@@ -42,15 +66,13 @@ test("#somar", () => {
   expect(math.somar(1, 2)).resolves.toBe(3); //como é uma promessa, é necessário o resolves.
 
   // criando o mock e substituindo a implementação para uma subtração
-  const mockSomar = jest
-    .spyOn(math, "somar")
-    .mockImplementation((a, b) => a - b);
+  jest.spyOn(math, "somar").mockImplementation((a, b) => a - b);
 
   math.somar(5, 1);
-  expect(mockSomar).toHaveBeenCalledTimes(1);
-  expect(mockSomar(5, 1)).toBe(4);
-  expect(mockSomar).toHaveBeenCalledTimes(2);
-  expect(mockSomar).toHaveBeenLastCalledWith(5, 1);
+  expect(math.somar).toHaveBeenCalledTimes(1);
+  expect(math.somar(5, 1)).toBe(4);
+  expect(math.somar).toHaveBeenCalledTimes(2);
+  expect(math.somar).toHaveBeenLastCalledWith(5, 1);
 
   // restaurando a implementação original
   math.somar.mockRestore();
