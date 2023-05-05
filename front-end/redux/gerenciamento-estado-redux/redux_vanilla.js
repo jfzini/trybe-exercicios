143c2db5fa910f039ea8274d6d8d626c2ed914b4
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-05 09:41:01 -0300

    incrementing global state

--- front-end/redux/gerenciamento-estado-redux/redux_vanilla.js
+++ front-end/redux/gerenciamento-estado-redux/redux_vanilla.js
@@ -1,6 +1,28 @@
-const incBtn = document.querySelector('button');
-const incText = document.querySelector('h2');
+import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
+const INITIAL_STATE = { count: 0 };
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case 'INC_COUNTER':
+      return { count: state.count + 1 };
+    default:
+      break;
+  }
+  return state;
+};
+
+const store = createStore(reducer, composeWithDevTools());
+
+const action = { type: 'INC_COUNTER' };
+const incBtn = document.querySelector('button');
 incBtn.addEventListener('click', () => {
-  incText.innerHTML = Number(incText.innerHTML) + 1;
+  store.dispatch(action);
 });
+
+// const incBtn = document.querySelector('button');
+// const incText = document.querySelector('h2');
+
+// incBtn.addEventListener('click', () => {
+//   incText.innerHTML = Number(incText.innerHTML) + 1;
+// });
