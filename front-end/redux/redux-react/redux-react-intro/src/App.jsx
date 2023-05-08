24765c8ac16b97e2ec33e1c7050fcbe1a5cafa1a
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-08 16:35:07 -0300

    changing from CRA to vite

--- front-end/redux/redux-react/redux-react-intro/src/App.jsx
+++ front-end/redux/redux-react/redux-react-intro/src/App.jsx
@@ -7,8 +7,8 @@ function App({ countState, dispatch }) {
     <div className="App">
       <h2>Contador</h2>
       <p>{countState}</p>
-      <button onClick={dispatch(actionCreator())}>Incrementar 1</button>
-      <button onClick={dispatch(actionCreator(5))}>Incrementar 5</button>
+      <button onClick={() => dispatch(actionCreator())}>Incrementar 1</button>
+      <button onClick={() => dispatch(actionCreator(5))}>Incrementar 5</button>
     </div>
   );
 }
