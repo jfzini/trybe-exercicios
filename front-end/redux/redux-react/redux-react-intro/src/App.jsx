1c48157050de50236e9a89042110e5949692f770
João Felipe Zini <joaofelipezini@gmail.com>
2023-05-09 01:06:53 -0300

    dispatchAll

--- front-end/redux/redux-react/redux-react-intro/src/App.jsx
+++ front-end/redux/redux-react/redux-react-intro/src/App.jsx
@@ -1,20 +1,27 @@
 import './App.css';
 import { connect } from 'react-redux';
-import { actionCreator } from './redux/actions';
+import { actionCreator, clickCounter } from './redux/actions';
 
-function App({ countState, dispatch }) {
+function App({ countState, clickState, dispatch }) {
+  const dispatchAll = (num) => {
+    dispatch(actionCreator(num));
+    dispatch(clickCounter());
+  };
+
   return (
     <div className="App">
       <h2>Contador</h2>
       <p>{countState}</p>
-      <button onClick={() => dispatch(actionCreator())}>Incrementar 1</button>
-      <button onClick={() => dispatch(actionCreator(5))}>Incrementar 5</button>
+      <button onClick={() => dispatchAll()}>Incrementar 1</button>
+      <button onClick={() => dispatchAll(5)}>Incrementar 5</button>
+      <p>Você já clicou no botão {clickState} vezes</p>
     </div>
   );
 }
 
 const mapStateToProps = (state) => ({
   countState: state.count,
+  clickState: state.clicks,
 });
 
 export default connect(mapStateToProps)(App);
--- front-end/redux/redux-react/redux-react-intro/src/redux/reducers/counterReducer.js
+++ front-end/redux/redux-react/redux-react-intro/src/redux/reducers/counterReducer.js
@@ -1,9 +1,11 @@
-const INITIAL_STATE = { count: 0 };
+const INITIAL_STATE = { count: 0, clicks: 0 };
 
 const counterReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'INCREMENT_COUNTER':
-      return { count: state.count + action.payload };
+      return { ...state, count: state.count + action.payload };
+    case 'INCREMENT_CLICKS':
+      return { ...state, clicks: state.clicks + 1 }
     default:
       return state;
   }
